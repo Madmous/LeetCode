09f2bdcb21fc9858ca223d9ecb3d7c5ecca72e5a
Moustapha A Diouf <mail@madmous.com>
2017-02-28 13:21:58 +0100

    Add approach 4

--- 1.TwoSum.js
+++ 1.TwoSum.js
@@ -67,3 +67,22 @@ var twoSum = function(nums, target) {
     
     return number;
 };
+
+// Approach #4 - TC: O(n) ; SC = O(n)
+var twoSum = function(nums, target) {
+    let number = [];
+    let numByIndex = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        let numToFind = target - nums[i];
+        
+        if (numByIndex.hasOwnProperty(numToFind)) {
+            number = [numByIndex[numToFind], i];
+            break;
+        }
+        
+        numByIndex[nums[i]] = i;
+    }
+    
+    return number;
+};
